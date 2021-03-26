8faacf4078ebcde700f06c7a5a6cfde4ae84d96e
David <d.boutin84@gmail.com>
2021-03-26 17:24:48 -0400

    in progress

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,38 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import {Link, useStaticQuery, graphql} from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
+import ThemeChanger from "../components/themeChanger"
 
-export default ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
-  return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+
+export default ({children}) => {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `
+    )
+    return (
+        <div className="site-wrapper">
+            <Link to="/">
+                <img alt="header"
+                     src='https://mypodboxx.com/serious-plan/wp-content/uploads/sites/213/2020/10/podcastbanneronlyfinalversion-02.png'/>
+            </Link>
+            <header className="site-header">
+                <Navigation/>
+                <ThemeChanger/>
+            </header>
+            {children}
+            <footer className="site-footer">
+                <p>&copy; {new Date().getFullYear()} <a
+                    className="-primary">{data.site.siteMetadata.title}</a> &bull; Powered by &nbsp;
+                    <a target="_blank" href="https://www.podboxx.com">PodBoxx</a></p>
+            </footer>
         </div>
-        <Navigation />
-      </header>
-      {children}
-      <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
-      </footer>
-    </div>
-  )
+    )
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,18 @@
 import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import spotify from "super-tiny-icons/images/svg/spotify.svg";
+import apple from "super-tiny-icons/images/svg/itunes_podcasts.svg"
+import google from "super-tiny-icons/images/svg/google_podcasts.svg"
+import facebook from "super-tiny-icons/images/svg/facebook.svg"
+import twitter from "super-tiny-icons/images/svg/twitter.svg"
+
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
-  </nav>
-  
+    <div className="navigation">
+        <a href="https://open.spotify.com/show/2cbhkhbIIXulqxICRew3sL?si=uKeaaAcTRPye9JUqD8Ea2A"><img style={{width: 50}} alt="spotify" src={spotify}/></a>
+        <a href="https://podcasts.apple.com/us/podcast/parent-et-amis/id1493207704"><img style={{width: 50}} alt="apple" src={apple}/></a>
+        <a href="https://podcasts.google.com/?feed=aHR0cHM6Ly9hcGkucG9kYm94eC5jb20vY2hhbm5lbHMvMTk4L3Jzcw"> <img style={{width: 50}} alt="google" src={google}/></a>
+        <a href="https://facebook.com/podboxxmedia"><img style={{width: 50}} alt="facebook" src={facebook}/></a>
+        <a href="https://twitter.com"><img style={{width: 50}} alt="facebook" src={twitter}/></a>
+    </div>
+
 )
